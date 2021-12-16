dc55aca3caf30b145536968985de55d28e0b04a6
Michalczyk <dawid.michalczyk@capgemini.com>
2021-12-16 20:32:09 +0100

    First view created

--- config/urls.py
+++ config/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+# the 'include()' method allow to link another urls.py file created in your app folder (app_name/urls.py)
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls), #pre-created admin urls routes
+    path('', include('micelka.urls')) # include app urls
 ]
--- micelka/views.py
+++ micelka/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """The home page for Micelka."""
+    return render(request,
+        'micelka/index.html')
