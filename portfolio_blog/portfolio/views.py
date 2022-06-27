34bf2167e1a79698b76ca7e7be89228342462cd3
Michalczyk <dawid.michalczyk@capgemini.com>
2022-06-27 23:57:34 +0200

    project content aggregation added

--- portfolio_blog/portfolio/views.py
+++ portfolio_blog/portfolio/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from . models import Project
 
 # Create your views here.
 def homepage(response):
-    return render(response, "theme/base.html", {})
+    return render(response, "portfolio/base.html", {})
+
+def portfolio(request):
+    projects = Project.objects.all()
+    project = {
+        "projects" : projects
+    }
+    return render(request, "portfolio/home.html", project)
+    
+    
