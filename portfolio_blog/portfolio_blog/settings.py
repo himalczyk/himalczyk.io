9b77bfaae8ad480ebca056b3cc2fa7bbc99076e2
Michalczyk <dawid.michalczyk@capgemini.com>
2022-07-06 12:54:04 +0200

    css changes

--- portfolio_blog/portfolio_blog/settings.py
+++ portfolio_blog/portfolio_blog/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -132,6 +132,8 @@ TAILWIND_APP_NAME = 'theme'
 INTERNAL_IPS = [
     "127.0.0.1",
 ]
+
+TAILWIND_CSS_PATH = 'css/dist/styles.css'
 
 # this is fixing the pathing issue for being able to find node installed on pc
 NPM_BIN_PATH = r"C:\Program Files\nodejs\npm.cmd"
