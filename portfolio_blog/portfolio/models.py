94cc76cf367cde0a074b949b235937a66306dc63
Michalczyk <dawid.michalczyk@capgemini.com>
2022-07-10 18:57:43 +0200

    fixed images

--- portfolio_blog/portfolio/models.py
+++ portfolio_blog/portfolio/models.py
@@ -4,7 +4,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.FilePathField(path="portfolio/static/img")
+    image = models.ImageField(upload_to="images/")
     
     def __str__(self):
         return self.title
