2c8cbdf43eee6a231052a1b957b336b55c1f6be0
Michalczyk <dawid.michalczyk@capgemini.com>
2022-07-28 00:01:03 +0200

    added new sub-category

--- portfolio_blog/api/urls.py
+++ portfolio_blog/api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("api/", views.api_home, name="api_home"),
-    path("rp_tutorial/", views.rp_article_index, name="rp_article_index"),
+    path("podcasts/", views.podcasts, name="podcasts"),
     path("yt_channel_tutorials/", views.yt_video_index, name="yt_video_index"),
 ]
--- portfolio_blog/api/views.py
+++ portfolio_blog/api/views.py
@@ -5,17 +5,6 @@ from api.web_scraper import scrape_rp_image_uri
 
 # Create your views here.
 def api_home(response):
-    return render(response, 'api/api_home.html', {})
-
-def yt_video_index(response):
-    yt_api = YoutubeApi(YT_BASE_URL, YOUTUBE_CHANNELS)
-    yt_videos = yt_api.get_youtube_urls()
-    video = {
-        "yt_videos" : yt_videos,
-    }
-    return render(response, "api/yt_video_index.html", video)
-
-def rp_article_index(response):
     rp_post_uri = scrape_rp_image_uri()[1]
     rp_post_image = str(scrape_rp_image_uri()[0])
     rp_post_image_alt = str(scrape_rp_image_uri()[2])
@@ -26,4 +15,15 @@ def rp_article_index(response):
         "alt" : rp_post_image_alt,
         "description" : rp_post_description,
     }
-    return render(response, "api/rp_article_index.html", blog_post)
+    return render(response, "api/api_home.html", blog_post)
+
+def yt_video_index(response):
+    yt_api = YoutubeApi(YT_BASE_URL, YOUTUBE_CHANNELS)
+    yt_videos = yt_api.get_youtube_urls()
+    video = {
+        "yt_videos" : yt_videos,
+    }
+    return render(response, "api/yt_video_index.html", video)
+
+def podcasts(response):
+    return render(response, "api/podcasts.html", {})
