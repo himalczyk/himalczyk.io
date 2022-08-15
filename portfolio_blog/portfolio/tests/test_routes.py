1eddffedf043b94b3cb3f74d82791b07d43c4a01
Michalczyk <dawid.michalczyk@capgemini.com>
2022-08-15 14:07:58 +0200

    styling and tests

--- portfolio_blog/portfolio/tests/test_routes.py
+++ portfolio_blog/portfolio/tests/test_routes.py
@@ -4,15 +4,6 @@ from django.urls import reverse
 
 
 class RouteTest(TestCase):
-    
-    @pytest.mark.django_db
-    def test_about_page(self):
-        about_page_url = reverse("about")
-        
-        response = self.client.get(about_page_url)
-        
-        assert response.status_code == 200
-        assert response.context["request"].path == "/about/"
         
     @pytest.mark.django_db
     def test_home_page(self):
@@ -31,3 +22,32 @@ class RouteTest(TestCase):
         
         assert response.status_code == 200
         assert response.context["request"].path == "/portfolio/"
+    
+    @pytest.mark.django_db
+    def test_tutorial_page(self):
+        tutorial_page_url = reverse("tutorial")
+        
+        response = self.client.get(tutorial_page_url)
+        
+        assert response.status_code == 200
+        assert response.context["request"].path == "/tutorial/"
+        
+    @pytest.mark.django_db
+    def test_api_page(self):
+        api_page_url = reverse("api_home")
+        
+        response = self.client.get(api_page_url)
+        
+        assert response.status_code == 200
+        assert response.context["request"].path == "/api/"
+        
+    @pytest.mark.django_db
+    def test_about_page(self):
+        about_page_url = reverse("about")
+        
+        response = self.client.get(about_page_url)
+        
+        assert response.status_code == 200
+        assert response.context["request"].path == "/about/"
+        
+    
