085fd76998ad67823d9c6e9df6bbf086a401458d
Michalczyk <dawid.michalczyk@capgemini.com>
2022-11-26 19:02:24 +0100

    routing fixed

--- portfolio_blog/followed_sources/urls.py
+++ portfolio_blog/followed_sources/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("followed_sources/", views.followed_sources_home, name="followed_sources_home"),
     path("podcasts/", views.podcasts, name="podcasts"),
-    path("videos/", views.rp_videos, name="videos"),
-    path("yt_channel_tutorials/", views.yt_video_index, name="yt_video_index"),
+    path("articles/", views.rp_articles, name="rp_articles"),
+    path("videos/", views.yt_video_index, name="yt_video_index"),
 ]
